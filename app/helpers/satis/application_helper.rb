6701b5fec3f254ddf092b04f63ea17f7fb9c2ae3
Tom de Grunt <tom@degrunt.nl>
2021-06-24 20:09:54 +0200

    Don't instantiate everytime

--- app/helpers/satis/application_helper.rb
+++ app/helpers/satis/application_helper.rb
@@ -1,7 +1,7 @@
 module Satis
   module ApplicationHelper
     def satis
-      @container = Satis::Helpers::Container.new(self)
+      @container ||= Satis::Helpers::Container.new(self)
     end
   end
 end
