9d0f7dab7eaaf81ebcc4e55b1bce74b02e9e9617
Tom de Grunt <tom@degrunt.nl>
2021-06-25 19:51:44 +0200

    javascript stuff

---
+++ frontend/javascript/controllers/application_controller.js
@@ -0,0 +1,18 @@
+// application_controller.js
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  getController(element, identifier) {
+    return this.application.getControllerForElementAndIdentifier(element, identifier)
+  }
+
+  triggerEvent(el, name, data) {
+    if (typeof window.CustomEvent === "function") {
+      let event = new CustomEvent(name, { detail: data })
+    } else {
+      let event = document.createEvent("CustomEvent")
+      event.initCustomEvent(name, true, true, data)
+    }
+    el.dispatchEvent(event)
+  }
+}
