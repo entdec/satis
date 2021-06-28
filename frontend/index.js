8141485c6f5b8a394e9158d0a4bd908fae501099
Tom de Grunt <tom@degrunt.nl>
2021-06-28 13:24:11 +0200

    Try

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,19 @@
-export { Satis } from "./javascript/satis"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Application } from "stimulus"
+
+export class Satis {
+  static start(application) {
+    if (!application) {
+      application = Application.start()
+    }
+
+    console.log("Satis")
+
+    this.application = application
+    const context = require.context("./javascript/controllers", true, /\.js$/)
+    // this.application.load(definitionsFromContext(context))
+
+    const contextComponents = require.context("../../app/components/", true, /_controller\.js$/)
+    this.application.load(definitionsFromContext(context).concat(definitionsFromContext(contextComponents)))
+  }
+}
