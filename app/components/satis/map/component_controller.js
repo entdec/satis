463c729c3f29b64032934a05aec1eda9b1884a8e
Andre Meij <andre@itsmeij.com>
2021-07-02 08:57:42 +0200

    Usefull comments

--- app/components/satis/map/component_controller.js
+++ app/components/satis/map/component_controller.js
@@ -9,6 +9,8 @@ export default class extends Controller {
   static values = { urls: String, latitude: Number, longitude: Number, zoomLevel: Number, geoJsonUrl: String }
 
   connect() {
+    // Example https://leafletjs.com/examples/choropleth/
+    // Data https://public.opendatasoft.com/explore/?sort=modified&q=netherlands
 
     this.map = L.map(this.containerTarget).setView([this.latitudeValue, this.longitudeValue], this.zoomLevelValue);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(this.map);
@@ -19,8 +21,6 @@ export default class extends Controller {
     fetch(this.geoJsonUrlValue)
       .then(response => response.json())
       .then(data => {
-        console.log(this.geoJsonUrlValue, "loading data", data)
-
         L.geoJSON(data, {
           onEachFeature: (feature, layer) => {
             layer.on('click', () => this.onClick(layer))
