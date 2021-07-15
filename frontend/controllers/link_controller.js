37f26429d6df8ef66bd1977393926c3659b7bfa2
Tom de Grunt <tom@degrunt.nl>
2021-07-15 13:25:44 +0200

    Row links

---
+++ frontend/controllers/link_controller.js
@@ -0,0 +1,15 @@
+import ApplicationController from "./application_controller"
+
+/*
+ * Theme controller
+ *
+ *   div data-controller="satis-link" href="" data-action='click->satis-link#select'
+ *
+ */
+export default class extends ApplicationController {
+  connect() {}
+
+  select() {
+    Turbo.visit(this.element.getAttribute("href"))
+  }
+}
