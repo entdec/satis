ef21cfdb1b4f7e455df17cf5ff40bfd4468fd0ec
Tom de Grunt <tom@degrunt.nl>
2021-07-23 21:41:04 +0200

    Don't think we'll use this anymore

--- app/components/satis/ui_table/helpers.rb
+++
@@ -1,9 +0,0 @@
-module Satis
-  module UiTable
-    module Helpers
-      def ui_table(*args, &block)
-        action_view.render(Satis::Table::Component.new(*args), &block)
-      end
-    end
-  end
-end
