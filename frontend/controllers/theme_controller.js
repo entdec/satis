12ac9045f719b214bfc85e2a165ad225cc54ba52
Tom de Grunt <tom@degrunt.nl>
2021-07-26 12:16:58 +0200

    Only delay when manually trigged

--- frontend/controllers/theme_controller.js
+++ frontend/controllers/theme_controller.js
@@ -15,34 +15,42 @@ export default class extends ApplicationController {
     super.connect()
 
     const theme = getInitialTheme()
-    this.rawSetTheme(theme)
+    this.rawSetTheme(theme, false)
   }
 
   switch() {
     const theme = getInitialTheme()
     if (theme == "dark") {
-      this.rawSetTheme("light")
+      this.rawSetTheme("light", true)
     } else {
-      this.rawSetTheme("dark")
+      this.rawSetTheme("dark", true)
     }
   }
 
-  rawSetTheme(rawTheme) {
+  rawSetTheme(rawTheme, delay) {
     const root = window.document.documentElement
     const isDark = rawTheme === "dark"
 
+    if (delay == true) {
+      this.lightTarget.classList.add("transition", "ease-in-out", "duration-1000")
+      this.darkTarget.classList.add("transition", "ease-in-out", "duration-1000")
+    }
+
     if (isDark) {
-      this.lightTarget.classList.add("transition", "ease-in-out", "duration-1000", "transform", "translate-y-7")
-      this.darkTarget.classList.add("transition", "ease-in-out", "duration-1000", "transform", "-translate-y-7")
+      this.lightTarget.classList.add("transform", "translate-y-7")
+      this.darkTarget.classList.add("transform", "-translate-y-7")
     } else {
       this.lightTarget.classList.remove("transform", "translate-y-7")
       this.darkTarget.classList.remove("transform", "-translate-y-7")
     }
 
     localStorage.setItem("color-theme", rawTheme)
-    setTimeout(() => {
-      root.classList.remove(isDark ? "light" : "dark")
-      root.classList.add(rawTheme)
-    }, 1000)
+    setTimeout(
+      () => {
+        root.classList.remove(isDark ? "light" : "dark")
+        root.classList.add(rawTheme)
+      },
+      delay ? 1000 : 0
+    )
   }
 }
