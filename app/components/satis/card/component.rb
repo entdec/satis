2238628e546ceda703c88a073a697cf949a4685b
Tom de Grunt <tom@degrunt.nl>
2021-07-27 13:30:03 +0200

    Allow for custom header backgrounds

--- app/components/satis/card/component.rb
+++ app/components/satis/card/component.rb
@@ -5,15 +5,20 @@ module Satis
       renders_many :tabs, Tab::Component
       renders_one :footer
 
-      attr_reader :icon, :title, :description, :menu, :content_padding
+      attr_reader :icon, :title, :description, :menu, :content_padding, :header_background_color
 
-      def initialize(icon: nil, title: nil, description: nil, menu: nil, content_padding: true)
+      def initialize(icon: nil, title: nil, description: nil, menu: nil, content_padding: true,
+                     header_background_color: {
+                       dark: 'bg-gray-800', light: 'bg-white'
+                     })
         super
         @title = title
+        @title = @title.join(' ') if @title.is_a?(Array)
         @description = description
         @icon = icon
         @menu = menu
         @content_padding = content_padding
+        @header_background_color = header_background_color
       end
 
       def tabs?
