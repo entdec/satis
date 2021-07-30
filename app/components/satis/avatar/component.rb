22da2b4c8600e03443ef8dc30952663d263aa722
Tom de Grunt <tom@degrunt.nl>
2021-07-30 14:05:37 +0200

    Add gravatar

--- app/components/satis/avatar/component.rb
+++ app/components/satis/avatar/component.rb
@@ -1,23 +1,48 @@
 module Satis
   module Avatar
     class Component < Satis::ApplicationComponent
-      attr_reader :name, :photo, :size_class
+      attr_reader :name, :photo, :size_class, :email
 
-      def initialize(name: nil, photo: nil, size_class: 'h-8 w-8')
+      def initialize(name: nil, email: nil, photo: nil, size_class: 'h-8 w-8')
         super
         @name = name
         @photo = photo
         @size_class = size_class
+        @email = email
       end
 
       def initials
-        name.scan(/[A-Z]/)[0..1].join
+        if name.present? && !name.index('@')
+          name.scan(/[A-Z]/)[0..1].join
+        else
+          (name || email).split('@').map(&:capitalize).join('@').scan(/[A-Z]/)[0..1].join
+        end
       end
 
       def photo_url
-        return unless photo.attached?
+        return unless photo&.attached?
 
         helpers.main_app.url_for(photo)
+      end
+
+      def gravatar?
+        return false if email.blank?
+
+        url = "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email).downcase}?d=404"
+
+        uri = URI.parse(url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true if uri.scheme == 'https'
+
+        request = Net::HTTP::Get.new(uri.request_uri)
+        request.add_field('User-Agent', controller.request.user_agent)
+        response = http.request(request)
+
+        response.code.to_i != 404
+      end
+
+      def gravatar_url
+        "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email).downcase}?d=404" if gravatar?
       end
     end
   end
