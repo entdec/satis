fbfd8b565497308010de0d04ab145b0959011662
Tom de Grunt <tom@degrunt.nl>
2021-08-02 14:19:50 +0200

    Reject empty

--- app/components/satis/card/component.rb
+++ app/components/satis/card/component.rb
@@ -15,7 +15,7 @@ module Satis
                      })
         super
         @title = title
-        @title = @title.compact.join(' ') if @title.is_a?(Array)
+        @title = @title.reject(&:empty?).compact.join(' ') if @title.is_a?(Array)
         @description = description
         @icon = icon
         @menu = menu
