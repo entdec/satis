c5e344b016d972b333d2b24b0ccb87996a19c755
Tom de Grunt <tom@degrunt.nl>
2021-08-03 11:03:27 +0200

    Allow shortcut for actions

--- app/components/satis/card/component.rb
+++ app/components/satis/card/component.rb
@@ -7,12 +7,17 @@ module Satis
       renders_many :tabs, Tab::Component
       renders_one :footer
 
-      attr_reader :icon, :title, :description, :menu, :content_padding, :header_background_color
+      attr_reader :icon, :title, :description, :menu, :content_padding, :header_background_color, :override_actions
 
-      def initialize(icon: nil, title: nil, description: nil, menu: nil, content_padding: true,
+      def initialize(icon: nil,
+                     title: nil,
+                     description: nil,
+                     menu: nil,
+                     content_padding: true,
                      header_background_color: {
                        dark: 'bg-gray-800', light: 'bg-white'
-                     })
+                     },
+                     actions: [])
         super
         @title = title
         @title = @title.reject(&:blank?).compact.join(' ') if @title.is_a?(Array)
@@ -21,6 +26,7 @@ module Satis
         @menu = menu
         @content_padding = content_padding
         @header_background_color = header_background_color
+        @override_actions = actions
       end
 
       def tabs?
