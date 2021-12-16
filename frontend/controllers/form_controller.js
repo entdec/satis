6134161b3598c8833c525428547f949c4be23f12
Mark Cornelissen <mark@boxture.com>
2021-12-16 06:41:45 +0100

    Implement a confirm window when leaving a window with unsaved changes

--- frontend/controllers/form_controller.js
+++ frontend/controllers/form_controller.js
@@ -3,7 +3,8 @@ import ApplicationController from "./application_controller"
 export default class extends ApplicationController {
   static targets = []
   static values = {
-    noSubmitOnEnter: Boolean
+    noSubmitOnEnter: Boolean,
+    confirmBeforeLeave: Boolean
   }
 
   connect() {
@@ -16,5 +17,24 @@ export default class extends ApplicationController {
         }
       })
     }
+
+    if (this.confirmBeforeLeaveValue) {
+      this.originalData = this.getFormData()
+      document.addEventListener('turbo:before-visit', this.checkForChanges.bind(this))
+    }
+  }
+
+  checkForChanges(event) {
+    if (this.isDirty() && ! window.confirm("Changes you made may not be saved, are you sure you want to leave this page?")) {
+      event.preventDefault()
+    }
+  }
+
+  getFormData() {
+    return new URLSearchParams(new FormData(this.element)).toString()
+  }
+
+  isDirty() {
+    return this.originalData != this.getFormData()
   }
 }
