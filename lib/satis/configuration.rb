bf23bbc22840d953e0f80ea959332fed063ea716
Tom de Grunt <tom@degrunt.nl>
2022-10-26 09:32:43 +0200

    Add logger config

--- lib/satis/configuration.rb
+++ lib/satis/configuration.rb
@@ -4,17 +4,19 @@ module Satis
   class Configuration
     attr_accessor :submit_on_enter, :confirm_before_leave
     attr_writer :default_help_text
+    attr_writer :logger
 
     def initialize
+      @logger = Logger.new(STDOUT)
       @submit_on_enter = true
       @confirm_before_leave = false
 
       @default_help_text = lambda do |_template, _object, key, _additional_scope|
         scope = help_scope(template, object, additional_scope)
 
-        value = I18n.t((['help'] + scope + [key.to_s]).join('.'))
+        value = I18n.t((["help"] + scope + [key.to_s]).join("."))
 
-        if value.match(/translation missing: (.+)/)
+        if /translation missing: (.+)/.match?(value)
           nil
         else
           value
@@ -25,7 +27,7 @@ module Satis
     def default_help_text(template, object, method, additional_scope)
       if @default_help_text.is_a?(Proc)
         instance_exec(template, object, method, additional_scope,
-                      &@default_help_text)
+          &@default_help_text)
       else
         @default_help_text
       end
@@ -33,7 +35,7 @@ module Satis
 
     # Maybe not the right place?
     def help_scope(template, object, additional_scope, action: nil)
-      scope = template.controller.controller_path.split('/')
+      scope = template.controller.controller_path.split("/")
       scope << (action || template.controller.action_name)
       scope << object.class.name.demodulize.tableize.singularize
 
