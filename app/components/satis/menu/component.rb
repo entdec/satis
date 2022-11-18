d89c4dce57820466fdd9ce8f9868b35d7ceee01b
Rupali Rajesh Parse <r.parse@rapidlogistics.nl>
2022-11-18 18:44:27 +0400

    on change filter icon modifications

--- app/components/satis/menu/component.rb
+++ app/components/satis/menu/component.rb
@@ -4,12 +4,13 @@ module Satis
   module Menu
     class Component < Satis::ApplicationComponent
       # renders_many :tabs, Tab::Component
-      attr_reader :menu, :icon
+      attr_reader :menu, :icon, :selected_column
 
-      def initialize(menu, icon: nil)
+      def initialize(menu, icon: nil, selected_column: nil)
         super
         @menu = menu
         @icon = icon || 'fa-solid fa-ellipsis-vertical'
+        @selected_column = selected_column
       end
     end
   end
