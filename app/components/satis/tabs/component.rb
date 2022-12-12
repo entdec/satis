74c1742a25079a272e58444820dab82b25bf9e8d
Rupali Rajesh Parse <r.parse@rapidlogistics.nl>
2022-12-12 12:48:18 +0400

    Added multiple view functionality

--- app/components/satis/tabs/component.rb
+++ app/components/satis/tabs/component.rb
@@ -4,14 +4,25 @@ module Satis
   module Tabs
     class Component < Satis::ApplicationComponent
       renders_many :tabs, Tab::Component
-      attr_reader :group, :persist, :key
+      attr_reader :group, :persist, :action_table_view, :key
 
-      def initialize(group: :main, persist: false, key: nil)
+      def initialize(group: :main, persist: false, action_table_view: false, key: nil)
         super
         @group = group
         @persist = persist
+        @action_table_view = action_table_view
         @key = key
       end
+
+      def view_menu(view_id)
+        Satis::Menus::Builder.build(:view_menu) do |m|
+          m.item :reset, icon: 'fa-solid fa-xmark', link: nil, link_attributes: {data: { reflex: 'click->ActionTable::FilterReflex#reset_filters', 'reflex-serialize-form': "true", table: params[:table], view_id: view_id} }
+          m.item :save_view, icon: 'fa-regular fa-floppy-disk', link: nil, link_attributes: { data: { reflex: 'click->ActionTable::FilterReflex#save_filters', 'reflex-serialize-form': "true", table: params[:table], view_id: view_id} }
+          m.item :delete_view, icon: 'fa-solid fa-trash-can', link: nil, link_attributes: { data: { reflex: 'click->ActionTable::FilterReflex#delete_view', 'reflex-serialize-form': "true", table: params[:table], view_id: view_id} }
+          m.item :duplicate_view, icon: 'fa-regular fa-copy', link: nil, link_attributes: { data: { reflex: "click->ActionTable::FilterReflex#duplicate_view", 'reflex-serialize-form': "true", table: params[:table], view_id: view_id} }
+        end
+      end
+
     end
   end
 end
--- app/models/satis/user_data.rb
+++ app/models/satis/user_data.rb
@@ -1,9 +1,8 @@
 module Satis
   class UserData < ApplicationRecord
-    belongs_to :user
+    belongs_to :user, optional: true
 
-    validates :key, presence: true, uniqueness: true
-    validates :data, presence: true
+    validates :key, presence: true, uniqueness: { scope: :user_id, allow_nil: true }
 
     def self.keyed(key)
       find_or_create_by(key: key)
