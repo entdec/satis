c2ac72612e5e5cba63cfe89ebfc8c949750674f0
Rupali Rajesh Parse <r.parse@rapidlogistics.nl>
2022-12-16 12:18:30 +0400

    Added event for view menu

--- app/components/satis/tabs/component.rb
+++ app/components/satis/tabs/component.rb
@@ -15,7 +15,7 @@ module Satis
       end
 
       def view_menu(view_id)
-        Satis::Menus::Builder.build(:view_menu) do |m|
+        Satis::Menus::Builder.build(:view_menu, event: 'click') do |m|
           m.item :reset_view, icon: 'fa-light fa-circle-xmark', link: nil, link_attributes: {data: { reflex: 'click->ActionTable::FilterReflex#reset_filters', 'reflex-serialize-form': "true", table: params[:table], view_id: view_id} }
           m.item :save_view, icon: 'fa-regular fa-floppy-disk', link: nil, link_attributes: { data: { reflex: 'click->ActionTable::FilterReflex#save_filters', 'reflex-serialize-form': "true", table: params[:table], view_id: view_id} }
           m.item :rename, icon: 'fa-light fa-pen', link: nil, link_attributes: { data: { reflex: "click->ActionTable::FilterReflex#change_label_to_text", 'reflex-serialize-form': "true", table: params[:table], view_id: view_id} }
