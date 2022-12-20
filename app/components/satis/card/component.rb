940e68cce93e0e0397cd6c549239edf453cdd6c8
Rupali Rajesh Parse <r.parse@rapidlogistics.nl>
2022-12-20 23:30:40 +0400

    Added custom link handling for custom views

--- app/components/satis/card/component.rb
+++ app/components/satis/card/component.rb
@@ -7,7 +7,7 @@ module Satis
       renders_many :tabs, Tab::Component
       renders_one :footer
 
-      attr_reader :icon, :title, :description, :menu, :content_padding, :header_background_color, :initial_actions, :key, :custom_views
+      attr_reader :icon, :title, :description, :menu, :content_padding, :header_background_color, :initial_actions, :key, :custom_tabs_link_html
 
       def initialize(icon: nil,
                      title: nil,
@@ -18,8 +18,7 @@ module Satis
                        dark: 'bg-gray-800', light: 'bg-white'
                      },
                      actions: [],
-                     key: nil,
-                     custom_views: false)
+                     key: nil)
         super
         @title = title
         @title = @title.reject(&:blank?).compact.join(' ') if @title.is_a?(Array)
@@ -30,7 +29,11 @@ module Satis
         @header_background_color = header_background_color
         @initial_actions = actions
         @key = key
-        @custom_views = custom_views
+      end
+
+      def custom_tabs_link(&block)
+        return unless block_given?
+        @custom_tabs_link_html = block.call.html_safe
       end
 
       def tabs?
