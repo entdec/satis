644d550d5513b0218851eaf485f5db97282f8037
Tom de Grunt <tom@degrunt.nl>
2023-09-14 15:24:06 +0200

    Only show documentation in development/test mode

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Satis::Engine.routes.draw do
   resources :user_data, only: %i[show update]
-  namespace :documentation do
-    resources :avatars
-    resources :cards
-    resources :forms do
-      get "select", on: :collection
+
+  if Rails.env.development? || Rails.env.test?
+    namespace :documentation do
+      resources :avatars
+      resources :cards
+      resources :forms do
+        get "select", on: :collection
+      end
     end
+    resources :documentation
   end
-  resources :documentation
 end
