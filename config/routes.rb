e00ebbef02c5b67f17f04fd7baf4204555e1f9d1
Tom de Grunt <tom@degrunt.nl>
2023-09-14 15:24:06 +0200

    Don't show documentation in production mode

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Satis::Engine.routes.draw do
   resources :user_data, only: %i[show update]
-  namespace :documentation do
-    resources :avatars
-    resources :cards
-    resources :forms do
-      get "select", on: :collection
+
+  unless Rails.env.production?
+    namespace :documentation do
+      resources :avatars
+      resources :cards
+      resources :forms do
+        get "select", on: :collection
+      end
     end
+    resources :documentation
   end
-  resources :documentation
 end
