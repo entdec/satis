7a48517daf2f4b6375afb531c1b20dc1371c9dd0
Tom de Grunt <tom@degrunt.nl>
2023-09-18 14:17:02 +0200

    Add type to UserData, so we can make derived classes

--- test/models/satis/user_data_test.rb
+++ test/models/satis/user_data_test.rb
@@ -9,16 +9,32 @@ module Satis
     test "can create user-data" do
       user = users(:one)
       data = user.user_data.keyed(:test)
-      data.data = {test: 'test'}
+      data.data = { test: "test" }
       assert data.save!
     end
 
     test "can create user-data, using derived classes" do
-      vd = ViewData.new(user: users(:one), key: 'test')
-      vd.test = 'test'
+      vd = ViewData.new(user: users(:one), key: "test")
+      vd.test = "test"
       vd.save!
 
-      assert_equal 'test', vd.test
+      assert_equal "test", vd.test
+      assert_equal "ViewData", vd.type
+    end
+
+    test "can find user-data using derived classes" do
+      vd = ViewData.new(user: users(:one), key: "test")
+      vd.test = "view_data"
+      vd.save!
+
+      vd = UserData.new(user: users(:one), key: "test")
+      vd.data = { test: "test" }
+      vd.save!
+
+      vd = ViewData.keyed('test')
+
+      assert_equal "view_data", vd.test
+      assert_equal "ViewData", vd.type
     end
   end
 end
