97476a362530a7a75ad1cb9dab037b6d6244c98d
Joyie Echteld <69836334+plummip@users.noreply.github.com>
2023-11-20 17:02:34 +0100

    Remove close on additions as per suggestion tom. do not introduce more code.

--- frontend/utils.js
+++ frontend/utils.js
@@ -38,27 +38,4 @@ const popperSameWidth = {
   },
 }
 
-const isPointInsideElement = (element, clientX, clientY, options= null)=> {
-  if (!Number.isFinite(clientX) || !Number.isFinite(clientY)) return false
-  options = Object.assign({ bufferSize: 0}, options)
-
-  const bounds = element.getBoundingClientRect()
-  let insideBounds =
-    (clientX >= bounds.left - options.bufferSize) && (clientX < bounds.right + options.bufferSize) &&
-    (clientY >= bounds.top - options.bufferSize) && (clientY < bounds.bottom + options.bufferSize)
-
-  if (!insideBounds) {
-    const elements = document.elementsFromPoint(clientX, clientY)
-    for (let i = 0; i < elements.length; i++) {
-      const childElement = elements[i]
-      if(childElement.style.display === 'none' || childElement.style.visibility === 'hidden' || childElement.style.opacity === '0') continue;
-      insideBounds = element === childElement || element.contains(childElement)
-      if (insideBounds){
-        break
-      }
-    }
-  }
-  return insideBounds
-}
-
-export { debounce, getInitialTheme, popperSameWidth, isPointInsideElement }
+export { debounce, getInitialTheme, popperSameWidth }
