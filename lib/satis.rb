2bad3d4f9973e3fffc33f57c00e42249cd6ef46a
Tom de Grunt <tom@degrunt.nl>
2024-02-02 11:47:33 +0100

    Switch to a more-used gem

--- lib/satis.rb
+++ lib/satis.rb
@@ -6,7 +6,7 @@ require 'satis/active_record_helpers'
 require 'view_component'
 require 'browser'
 require 'browser/aliases'
-require 'ar_doc_store'
+require 'jsonb_accessor'
 
 Browser::Base.include(Browser::Aliases)
 
